074b440847fcc021e66a1455ace4634c1dfabc2a
刘补石 <liubsh2024@shanghaitech.edu.cn>
2025-02-17 11:29:20 +0800

    Update train_stage1.sh

--- train_stage1.sh
+++ train_stage1.sh
@@ -1,6 +1,5 @@
-CUDA_VISIBLE_DEVICES=5 python main_vit.py &
-CUDA_VISIBLE_DEVICES=4 python main_Uni.py &
-CUDA_VISIBLE_DEVICES=3 python main_MPSA.py &
-CUDA_VISIBLE_DEVICES=2 python main_Meta.py &
-CUDA_VISIBLE_DEVICES=1 python main_cn_large.py &
-wait
+python main_vit.py
+python main_Uni.py
+python main_MPSA.py
+python main_Meta.py
+python main_cn_large.py
